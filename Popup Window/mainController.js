c2aa3708b3ca7f267be1092053df821fcad6c8a9
xiong-ang <xiong.ang@foxmail.com>
2018-04-23 21:32:14 +0800

    Test $http for github repo info

--- Popup Window/mainController.js
+++ Popup Window/mainController.js
@@ -1,7 +1,7 @@
 var app = angular.module('myApp', []);
 
-app.controller('mainController', ['$scope',
-    function($scope) {
+app.controller('mainController', ['$scope', '$http',
+    function($scope, $http) {
         $scope.bHide=false;
 
         $scope.searchHub = function(){
@@ -14,11 +14,29 @@ app.controller('mainController', ['$scope',
         };
 
         $scope.keyword="";
-        $scope.repos=["tet1","test12"];
+        $scope.repos=[];
         $scope.getRespo=function()
         {
-            $scope.repos=["HTML5","Javascript","AngularJS","Angular2","BootStrap","Restful API"];
+            $http({
+                method: 'GET',
+                url: 'https://api.github.com/search/repositories?q='+$scope.keyword+' in:name&sort=stars'
+            }).then(function successCallback(response) {
+
+                for (let index = 0; index < 10; index++) {
+                    //console.log(response);
+                    $scope.repos.push(
+                    {
+                        name:response.data.items[index].full_name, 
+                        starCount:response.data.items[index].stargazers_count
+                    });    
+                }
+
+                }, function errorCallback(response) {
+                    alert("Http request failed");
+            });
         };
+
+        
 
     }
 ]);
--- Popup Window/readme.md
+++ Popup Window/readme.md
@@ -1,2 +1,3 @@
 # 用AngularJs的ng-show实现弹出          
-# 测试了BootStrap的一些样式        
+# 测试了BootStrap的一些样式       
+# 测试$http获得github一些仓库信息       
